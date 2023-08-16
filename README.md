d0284b6d9fb716f90b38c81ec18b138e32be1200
mcramileux <kristineramilo21@gmail.com>
2023-08-16 20:21:46 +1000

    used the mern-search-engine as starter code

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # mcramileux-final-project
 23rd Challenge: Final Project - MERN Stack Single-Page Application
+
+## User Story
+
+## Acceptance Criteria
+
+## Technologies Used
+- JavaScript
+- Material UI
+- Express.js
+- React
+- Node.js
+- Apollo Server
+- Apollo Client
+- Web Vitals
+- GraphQL
+- JSON Web Tokens
+- Heroku
+- MongoDB
+  
+## Installation and Usage
+
+## Screenshot
+
+## Links
+Github Repository:
+Heroku Deployment:
+
+## References
+[Material UI](https://mui.com/material-ui/getting-started/installation/)
+
+## Contributions
+Contributions to this project won't be accepted as this is the reflection of the author's work hence the following questions and answers. Furthermore, forking or creating a pull request is acceptable.
+
+## Questions
+For more questions or inquiries, please contact the author at [GitHub](https://github.com/mcramileux) or email kristineramilo21@gmail.com.
+
+## License
+This project is under [MIT](https://choosealicense.com/licenses/mit/) license.
+
+## Author
+© 2023 mcramileux 
---
+++ music-sphere/client/src/components/Footer.js
@@ -0,0 +1,54 @@
+// TO EDIT; ADD MUI
+
+// import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faGithub } from '@fortawesome/free-brands-svg-icons';
+// import './style.css';
+
+// const Footer = () => {
+//   return (
+//     <footer className="footer mt-auto py-3 bg-dark text-white">
+//       <div className="container">
+//         <section className="social-icons-wrapper d-flex flex-column flex-md-row justify-content-center my-3">
+//           {/* JOCELYN'S SECTION */}
+//           <div className="d-flex flex-column align-items-center mx-md-2 transparent-bg">
+//             <a href="https://github.com/jjocelynn" className="icon jjocelynn" target="_blank" rel="noreferrer">
+//             <FontAwesomeIcon icon={faGithub} flip size="3x" style={{ color: 'green' }} />
+//             </a>
+//             <a href="https://github.com/jjocelynn" target="_blank" rel="noreferrer">
+//               <span className="d-inline-block mx-1 name">@jjocelynn</span>
+//             </a>
+//           </div>
+//           {/* DAVES SECTION */}
+//           <div className="d-flex flex-column align-items-center mx-md-2 transparent-bg">
+//             <a href="https://github.com/devarsh2395" className="icon devarsh2395" target="_blank" rel="noreferrer">
+//               <FontAwesomeIcon icon={faGithub} beat size="3x" style={{ color: 'purple' }} />
+//             </a>
+//             <a href="https://github.com/devarsh2395" target="_blank" rel="noreferrer">
+//               <span className="d-inline-block mx-1 name">@devarsh2395</span>
+//             </a>
+//           </div>
+//           {/* THOMAS' SECTION */}
+//           <div className="d-flex flex-column align-items-center mx-md-2 transparent-bg">
+//             <a href="https://github.com/ThomasCalle" className="icon ThomasCalle" target="_blank" rel="noreferrer">
+//               <FontAwesomeIcon icon={faGithub} bounce size="3x" style={{ color: 'orange' }} />
+//             </a>
+//             <a href="https://github.com/ThomasCalle" target="_blank" rel="noreferrer">
+//             <span className="d-inline-block mx-1 name">@ThomasCalle</span>
+//             </a>
+//           </div>
+//         </section>
+//         <section className="footer" id="footer">
+//           <section className="form-footer">
+//             <h5 className="text-center mb-2">Designed with ⏳ by UTOR PRJ3 T8</h5>
+//             <p className="text-center mb-0">
+//               <h6>&copy; {new Date().getFullYear()} All rights reserved.</h6>
+//             </p>
+//           </section>
+//         </section>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
